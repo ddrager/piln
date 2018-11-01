d6efcb28230fec79b5a0860f10b43287f2ea47d4
fiatjaf <fiatjaf@gmail.com>
2018-11-01 01:54:33 +0000

    getting invoice from frontend.

--- client/Main.js
+++ client/Main.js
@@ -7,36 +7,95 @@ import React, {useState, useEffect} from 'react' // eslint-disable-line no-unuse
 
 export default function Main() {
   let [objects, setObjects] = useState([])
+  let [selected, select] = useState(null)
 
-  useEffect(() => {
-    fetchObjects()
-      .then(setObjects)
-      .catch(toastError)
+  useEffect(async () => {
+    setObjects(await fetchObjects())
   }, [])
 
   return (
     <>
       <ToastContainer />
+      <AddPin cid={selected} />
       {objects.map(o => (
-        <div>JSON.stringify(o)</div>
+        <div
+          onClick={() => {
+            select(o)
+          }}
+        >
+          JSON.stringify(o)
+        </div>
       ))}
     </>
   )
 }
 
-function toastError(err) {
-  toast(err.message, {
-    type: 'error'
-  })
+function AddPin({object}) {
+  object = object || {cid: ''}
+
+  let [cid, cidUpdate] = useState(object.cid)
+  let [amount, amountUpdate] = useState(77)
+  let [note, noteUpdate] = useState('')
+
+  return (
+    <form
+      id="pin"
+      onSubmit={async e => {
+        e.preventDefault()
+
+        let invoice = await createOrder({cid, amount, note})
+        toast(invoice)
+      }}
+    >
+      <label>
+        <span>IPFS identifier:</span>{' '}
+        <input value={cid} onChange={setFromChange(cidUpdate)} />
+      </label>
+      <label>
+        <span>Satoshis to pay:</span>{' '}
+        <input value={amount} onChange={setFromChange(amountUpdate)} />
+      </label>
+      <label>
+        <span>Note to identify the object:</span>{' '}
+        <input value={note} onChange={setFromChange(noteUpdate)} />
+      </label>
+      <button>Pin</button>
+    </form>
+  )
+}
+
+function setFromChange(fn) {
+  return e => {
+    fn(e.target.value)
+  }
+}
+
+async function createOrder({cid, note, amount}) {
+  try {
+    let res = await fetch('/api/order', {
+      method: 'POST',
+      body: JSON.stringify({cid, note, amount}),
+      headers: {'Content-Type': 'application/json'}
+    })
+    if (!res.ok) throw new Error(await res.text())
+    return res.json()
+  } catch (err) {
+    console.error(err)
+    toast('failed to create order: ' + err.message, {
+      type: 'error'
+    })
+  }
 }
 
 async function fetchObjects() {
   try {
     let res = await fetch('/api/objects')
-    let r = await res.json()
-    return r
+    if (!res.ok) throw new Error(await res.text())
+    return res.json()
   } catch (err) {
     console.error(err)
-    throw new Error('failed to fetch objects')
+    toast('failed to fetch objects: ' + err.message, {
+      type: 'error'
+    })
   }
 }
--- lightning.go
+++ lightning.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -38,12 +39,13 @@ func makeInvoice(cid string, note string, amount int64) (string, error) {
 	callback_url := s.ServiceURL + "/callback/order"
 	order_id := cuid.New()
 
-	req, _ := on.Post("/charges").BodyJSON(struct {
+	req, _ := on.Post("/v1/charges").BodyJSON(struct {
 		Description string `json:"description"`
 		Amount      int64  `json:"amount"`
 		OrderId     string `json:"order_id"`
 		CallbackURL string `json:"callback_url"`
 	}{description, amount, order_id, callback_url}).Request()
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
@@ -52,6 +54,13 @@ func makeInvoice(cid string, note string, amount int64) (string, error) {
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
+	}
+
+	success := gjson.GetBytes(body, "success")
+	if success.Exists() && success.Bool() == false {
+		log.Error().Str("err", gjson.GetBytes(body, "message").String()).
+			Msg("failed to make invoice")
+		return "", errors.New("failed to make invoice")
 	}
 
 	invoice := gjson.GetBytes(body, "data.lightning_invoice.payreq").String()
